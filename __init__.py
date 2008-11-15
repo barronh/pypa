513dcd326d998a4e5c2c63d6d0574137dd000ec6
barronh <barronh@4a91f030-2914-0410-b58d-d885616ce41a>
2008-11-15 01:17:29 +0000

    Established moved testcase
    git-svn-id: https://dawes.sph.unc.edu/uncaqmlsvn/pyPA/trunk@577 4a91f030-2914-0410-b58d-d885616ce41a

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
 __all__=['utils','pappt','graphing','run','qa']
 from pyPA.pappt.loader import LoadPyPAFromYAML as run, LoadPAQAFromYAML as qa, template
-from pyPA.testcase.test import run as test
+try:
+	from pyPA.testcase.test import run as test
+except:
+	pass
