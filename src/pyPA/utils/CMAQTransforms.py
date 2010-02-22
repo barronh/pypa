fcfe65d93b0e54dc39168ab73781fba70692b8f0
barronh <barronh@4a91f030-2914-0410-b58d-d885616ce41a>
2010-02-22 16:26:31 +0000

    Added automatic subsetting for CMAQ concentration and meteorological files.  This helps to use standard model inputs to process subdomain Process Analysis
    git-svn-id: https://dawes.sph.unc.edu/uncaqmlsvn/pyPA/trunk@691 4a91f030-2914-0410-b58d-d885616ce41a

--- src/pyPA/utils/CMAQTransforms.py
+++ src/pyPA/utils/CMAQTransforms.py
@@ -1,5 +1,7 @@
 __all__ = ['time_avg_new_unit', 'wind_center_time_cell', 'pypass_cmaq_met_master', 'pypass_cmaq_emiss_master', 'MetaMetPlusAirMols', 'cmaq_pa_master']
 
+from datetime import datetime
+from warnings import warn
 from numpy import array,ones
 from PseudoNetCDF.MetaNetCDF import add_derived, \
                                   file_master
@@ -120,7 +122,7 @@ class MetaMetPlusAirMols(add_derived):
         airmols.var_desc='AIRMOLS'.ljust(16)
         return airmols
 
-def cmaq_pa_master(paths_and_readers,tslice=slice(None),kslice=slice(None),jslice=slice(None),islice=slice(None)):
+def cmaq_pa_master(paths_and_readers,tslice=None,kslice=None,jslice=None,islice=None):
     """
     CMAQ PA Master presents a single interface for CMAQ PA, IRR, and 
     Instantaneous concentration files.
@@ -135,15 +137,72 @@ def cmaq_pa_master(paths_and_readers,tslice=slice(None),kslice=slice(None),jslic
        tslice - slice object used to window the time period from the 
                 instantaneous concentration file to match the PA domain
        kslice - same as tslice, but for layers
-       jslice - same as tslice, but for rows
-       islice -  - same as tslice, but for columns
+       jslice - same as tslice, but for rows (defaults to an offset based on PA and conc difference)
+       islice -  - same as tslice, but for columns (defaults to an offset based on PA and conc difference)
               
     """
     from ..pappt.kvextract import tops2shape,pblhghts2tops
     files=[]
+    iprf = None
+    concf = None
     for p,r in paths_and_readers:
-        files.append(eval(r)(p))
+        thisf = eval(r)(p)
+        files.append(thisf)
+        if hasattr(thisf, 'FILEDESC'):
+            if iprf is None and ("Integrated Process Rates Output File" in thisf.FILEDESC or "Integrated Reaction Rates Output File" in thisf.FILEDESC):
+                iprf = thisf
+            if "Concentration file output" in thisf.FILEDESC:
+                concf = thisf
+
     master_file=file_master(files)
+    
+    if (iprf and concf) is not None:
+        ipr_date = datetime.strptime('%dT%06d' % (iprf.SDATE, iprf.STIME), '%Y%jT%H%M%S')
+        conc_date = datetime.strptime('%dT%06d' % (concf.SDATE, concf.STIME), '%Y%jT%H%M%S')
+        step_sec = (iprf.TSTEP / 10000) * 3600 + iprf.TSTEP % 10000 / 100 * 60 + iprf.TSTEP % 100
+        toffset = (ipr_date - conc_date).seconds / step_sec -1
+        tend = toffset + len(iprf.dimensions['TSTEP'])
+        tslice = tslice = slice(toffset, tend)
+        tmslice = slice(tslice.start, tslice.stop+1)
+        for vi, sigma in enumerate(concf.VGLVLS):
+            if sigma == iprf.VGLVLS[0]:
+                kslice = slice(vi, vi + iprf.NLAYS)
+                break
+        else:
+            warn("""Could not align concentration and ipr vertical levels:
+            conc: %s
+            ipr: %s
+            """ % (str(iprf.VGLVLS), str(concf.VGLVLS)))
+            kslice = kslice or slice(None)
+        
+        
+        xoffset = abs(concf.XORIG -  iprf.XORIG) / iprf.XCELL
+        assert(float(xoffset) == float(int(xoffset)))
+        xoffset = int(xoffset)
+        xend = iprf.NCOLS + xoffset
+        islice = islice or slice(xoffset, xend)
+        
+        yoffset = abs(concf.YORIG -  iprf.YORIG) / iprf.YCELL
+        assert(float(yoffset) == float(int(yoffset)))
+        yoffset = int(yoffset)
+        yend = iprf.NROWS + yoffset
+        jslice = jslice or slice(yoffset, yend)
+        
+        warn("""Automatically offseting based on subdomain.
+\ttstart: %d
+\ttend: %d
+\tzstart: %d
+\tzend: %d
+\txstart: %d
+\txend: %d
+\tystart: %d
+\tyend: %d""" % (tslice.start+1, tslice.stop, kslice.start+1, kslice.stop, islice.start+1, islice.stop, jslice.start+1, jslice.stop))
+    else:
+        warn("""Unable to identify CONC outputs and IPR outputs for automated slicing.
+                Looking at FILEDESC properties for "Integrated Process Rates Output File" or
+                "Integrated Reaction Rates Output File" and "Concentration file output"
+                """)
+
     def InitLambda(x,tslice,kslice,jslice,islice):
         return lambda self: PseudoIOAPIVariable(self,x,'f',('TSTEP','LAY','ROW','COL'),values=self.variables[x][:-1,:,:,:][tslice,kslice,jslice,islice],units=self.variables[x].units)
     def FinalLambda(x,tslice,kslice,jslice,islice):
@@ -159,6 +218,6 @@ def cmaq_pa_master(paths_and_readers,tslice=slice(None),kslice=slice(None),jslic
     master_file.addMetaVariable('CONC_AIRMOLS',lambda self: PseudoIOAPIVariable(self,'CONC_AIRMOLS','f',('TSTEP','LAY','ROW','COL'),values=CenterTime(self.variables['PRES'][:]/8.314472/self.variables['TA'][:])[tslice,kslice,jslice,islice],units='moles/m**3'))
     master_file.addMetaVariable('AIRMOLS',lambda self: PseudoIOAPIVariable(self,'AIRMOLS','f',('TSTEP','LAY','ROW','COL'),values=self.variables['CONC_AIRMOLS'][:]*self.XCELL*self.YCELL*2*CenterTime(self.variables['ZF'][:]-self.variables['ZH'][:])[tslice,kslice,jslice,islice],units='moles'))
     master_file.addMetaVariable('INVAIRMOLS',lambda self: PseudoIOAPIVariable(self,'INVAIRMOLS','f',('TSTEP','LAY','ROW','COL'),values=1/self.variables['AIRMOLS'][:],units='moles'))
-    master_file.addMetaVariable('DEFAULT_SHAPE',lambda self: PseudoIOAPIVariable(self,'DEFAULT_SHAPE','f',('TSTEP','LAY','ROW','COL'),values=tops2shape(pblhghts2tops(self.variables['PBL'][:],self.variables['ZF'][:]),self.variables['ZF'][:].shape)[tslice,kslice,jslice,islice],units='on/off'))
+    master_file.addMetaVariable('DEFAULT_SHAPE',lambda self: PseudoIOAPIVariable(self,'DEFAULT_SHAPE','f',('TSTEP','LAY','ROW','COL'),values=tops2shape(pblhghts2tops(self.variables['PBL'][:],self.variables['ZF'][:]),self.variables['ZF'][:].shape)[tmslice,kslice,jslice,islice],units='on/off'))
     
     return master_file
